19a0b795069e81b61389309b3fd4370365da7b58
Ru-Pirie <rubens.pirie@gmail.com>
2020-10-04 18:31:27 +0100

    Removed Not Needed Commands

--- commands/temp.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'temp',
-	description: 'Display info about yourself.',
-	execute(message) {
-            message.channel.send(`${message.mentions.members.first().roles.highest.id}`)
-	},
-};
